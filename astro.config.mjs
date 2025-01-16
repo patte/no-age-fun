5372e3bd687f27e5f81014b1f6e9311504772c56
patte <p@tte.io>
2025-01-16 16:42:54 +0100

    fix: inline stylsheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: "always",
+  },
   devToolbar: {
     enabled: false,
   },
